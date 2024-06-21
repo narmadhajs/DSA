14f5b9b24d06285fbd00931b558482280035651e
Narmadha J S <138014247+narmadhajs@users.noreply.github.com>
2024-06-21 17:33:33 +0530

    Update Bubblesort.cpp

--- Sorting Algoriths/Bubblesort.cpp
+++ Sorting Algoriths/Bubblesort.cpp
@@ -1,1 +1,32 @@
+#include<iostream>
+using namespace std;
+void bubble(int arr[],int n){
+    for(int i=0;i<n;i++){
+        bool swapped=false;
+        for(int j=0;j<n-i-1;j++){
+            if(arr[j]>arr[j+1]){
+                swap(arr[j],arr[j+1]);
+                swapped=true;
+            }
+        }
+    }
+}
+void print(int arr[],int n){
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+}
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
 
+    }
+    bubble(arr,n);
+    print(arr,n);
+
+}
+//input the n,arr[];
+//output the sorted arr
